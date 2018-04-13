658263ed05ac5d173021bda06bf802933b1a918d
nguyenkimlong <nguyenkimlong95@gmail.com>
2018-04-13 17:57:42 +0700

    push code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../Hero'
+import {Hero} from '../hero'
 import {HEROES} from '../mock-heroes'
+import {HeroService} from '../hero.service'
+import { OnChanges, AfterContentInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-heroes',
@@ -8,23 +10,23 @@ import {HEROES} from '../mock-heroes'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero={
-    id:1,
-    name:'nklong',
-  }
-  selectedHero:Hero;
-  
-  heroes = HEROES;
-  constructor() { }
-
-  ngOnInit() {
-   
-  }
-
  
-  onSelect(hero:Hero):void{
-   
-    this.selectedHero = hero;
-    console.log("selectedHero",this.selectedHero);
+   selectedHero:Hero;
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
+  ngOnInit() {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    debugger;
+    this.heroService.getHeroes().subscribe(heroes => this.heroes=heroes);
+    console.log(" this.heroes", this.heroes);
+  }
+ 
+  // onSelect(hero:Hero):void{
+   
+  //   this.selectedHero = hero;
+  //   console.log("selectedHero",this.selectedHero);
+  // }
 }
