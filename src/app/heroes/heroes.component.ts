c70f38b032be489a3e7c9bf9c7ef482f77d36fe7
nguyenkimlong <nguyenkimlong95@gmail.com>
2018-04-13 08:29:22 +0700

    build

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../Hero'
+import {HEROES} from '../mock-heroes'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero={
+    id:1,
+    name:'nklong',
+  }
+  selectedHero:Hero;
+  
+  heroes = HEROES;
+  constructor() { }
+
+  ngOnInit() {
+   
+  }
+
+ 
+  onSelect(hero:Hero):void{
+   
+    this.selectedHero = hero;
+    console.log("selectedHero",this.selectedHero);
+  }
+}
