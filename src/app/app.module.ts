301203bb6926dbc785043831cc4cda6e778879ab
vovanthinhit <vovanthinhit@gmail.com>
2018-04-24 17:13:23 +0700

    watch clock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { WatchComponent } from './watch/watch.component';
 
 
 
@@ -20,7 +21,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    WatchComponent
   ],
   imports: [
     BrowserModule,
